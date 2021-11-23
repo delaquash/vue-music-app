7d59a82e7111c893f1ed82c399fffae5e0b8f3a9
= <olaidemmanuel1@gmail.com>
2021-11-23 12:19:50 +0100

    fixed alert message for login

--- Desktop/music-app/src/components/Auth.vue
+++ Desktop/music-app/src/components/Auth.vue
@@ -45,29 +45,32 @@
               </ul>
 
               <!-- Login Form -->
-              <form v-show="tab === 'login'">
+              <vee-form :validation-schema="loginSchema"
+                v-show="tab === 'login'" @submit="login">
                 <!-- Email -->
                 <div class="mb-3">
                   <label class="inline-block mb-2">Email</label>
-                  <input type="email"
+                  <vee-field type="email" name="email"
                     class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition
                       duration-500 focus:outline-none focus:border-black rounded"
                     placeholder="Enter Email" />
+                    <ErrorMessage class="text-red-600" name="email" />
                 </div>
                 <!-- Password -->
                 <div class="mb-3">
                   <label class="inline-block mb-2">Password</label>
-                  <input type="password"
+                  <vee-field type="password"
                     class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition
                       duration-500 focus:outline-none focus:border-black rounded"
                     placeholder="Password" />
+                    <ErrorMessage class="text-red-600" name="password" />
                 </div>
                 <button type="submit"
                   class="block w-full bg-purple-600 text-white py-1.5 px-3 rounded transition
                     hover:bg-purple-700">
                   Submit
                 </button>
-              </form>
+              </vee-form>
 
               <!-- Registration Form -->
                <div class="text-white text-center font-bold p-5 mb-4"
@@ -181,6 +184,10 @@ export default {
       reg_show_alert: false,
       reg_alert_variant: 'bg-blue-500',
       reg_alert_msg: 'Please wait! Your account is being created.',
+      loginSchema: {
+        email: 'required|email',
+        password: 'required|min:8|max:36',
+      },
     };
   },
   computed: {
@@ -196,6 +203,8 @@ export default {
 
       this.reg_alert_variant = 'bg-green-500';
       this.reg_alert_msg = 'Success! Your account has been created.';
+    },
+    login(values) {
       console.log(values);
     },
   },
