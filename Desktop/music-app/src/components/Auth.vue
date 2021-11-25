bee16661743d6f24e378ee1df8bf35f649fc2d80
= <olaidemmanuel1@gmail.com>
2021-11-25 16:56:43 +0100

    added firebase as backend

--- Desktop/music-app/src/components/Auth.vue
+++ Desktop/music-app/src/components/Auth.vue
@@ -43,116 +43,8 @@
                     href="#">Register</a>
                 </li>
               </ul>
-
-              <!-- Login Form -->
-              <vee-form :validation-schema="loginSchema"
-                v-show="tab === 'login'" @submit="login">
-                <!-- Email -->
-                <div class="mb-3">
-                  <label class="inline-block mb-2">Email</label>
-                  <vee-field type="email" name="email"
-                    class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition
-                      duration-500 focus:outline-none focus:border-black rounded"
-                    placeholder="Enter Email" />
-                    <ErrorMessage class="text-red-600" name="email" />
-                </div>
-                <!-- Password -->
-                <div class="mb-3">
-                  <label class="inline-block mb-2">Password</label>
-                  <vee-field type="password"
-                    class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition
-                      duration-500 focus:outline-none focus:border-black rounded"
-                    placeholder="Password" />
-                    <ErrorMessage class="text-red-600" name="password" />
-                </div>
-                <button type="submit"
-                  class="block w-full bg-purple-600 text-white py-1.5 px-3 rounded transition
-                    hover:bg-purple-700">
-                  Submit
-                </button>
-              </vee-form>
-
-              <!-- Registration Form -->
-               <div class="text-white text-center font-bold p-5 mb-4"
-                v-if="reg_show_alert" :class="reg_alert_variant">
-                {{ reg_alert_msg }}
-              </div>
-              <vee-form v-show="tab === 'register'" :validation-schema="schema"
-                 @submit="register" :initial-values="userData">
-                <!-- Name -->
-                <div class="mb-3">
-                  <label class="inline-block mb-2">Name</label>
-                  <vee-field type="text" name="name"
-                    class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition
-                      duration-500 focus:outline-none focus:border-black rounded"
-                    placeholder="Enter Name" />
-                    <ErrorMessage class="text-red-600" name="name" />
-                </div>
-                <!-- Email -->
-                <div class="mb-3">
-                  <label class="inline-block mb-2">Email</label>
-                  <vee-field type="email" name="email"
-                    class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition
-                      duration-500 focus:outline-none focus:border-black rounded"
-                    placeholder="Enter Email" />
-                    <ErrorMessage class="text-red-600" name="email" />
-                </div>
-                <!-- Age -->
-                <div class="mb-3">
-                  <label class="inline-block mb-2">Age</label>
-                  <vee-field type="number" name="age"
-                    class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition
-                      duration-500 focus:outline-none focus:border-black rounded"
-                      placeholder="Enter Age" />
-                      <ErrorMessage class="text-red-600" name="age" />
-                </div>
-                <!-- Password -->
-                <div class="mb-3">
-                  <label class="inline-block mb-2">Password</label>
-                  <vee-field name="password" :bails="false" v-slot="{ field, errors }">
-                     <input type="password" class="block w-full py-1.5 px-3 text-gray-800 border
-                      border-gray-300 transition duration-500 focus:outline-none
-                      focus:border-black rounded" placeholder="Password" v-bind="field" />
-                    <div class="text-red-600" v-for="error in errors" :key="error">
-                      {{ error }}
-                    </div>
-                  </vee-field>
-                </div>
-                <!-- Confirm Password -->
-                <div class="mb-3">
-                  <label class="inline-block mb-2">Confirm Password</label>
-                  <vee-field type="password" name="confirm_password"
-                    class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition
-                      duration-500 focus:outline-none focus:border-black rounded"
-                    placeholder="Confirm Password" />
-                    <ErrorMessage  class="text-red-600" name="confirm_password" />
-                </div>
-                <!-- Country -->
-                <div class="mb-3">
-                  <label class="inline-block mb-2">Country</label>
-                  <vee-field as="select" name="country"
-                    class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition
-                      duration-500 focus:outline-none focus:border-black rounded">
-                    <option value="USA">USA</option>
-                    <option value="Mexico">Mexico</option>
-                    <option value="Germany">Germany</option>
-                    <option value="Pakistan">Pakistan</option>
-                  </vee-field>
-                  <ErrorMessage class="text-red-600" name="country" />
-                </div>
-                <!-- TOS -->
-                <div class="mb-3 pl-6">
-                  <vee-field name="tos" value='1' type="checkbox"
-                  class="w-4 h-4 float-left -ml-6 mt-1 rounded" />
-                  <label class="inline-block">Accept terms of service</label>
-                  <ErrorMessage class="text-red-600" name="tos" />
-                </div>
-                <button type="submit" :disabled="reg_in_submission"
-                  class="block w-full bg-purple-600 text-white py-1.5 px-3 rounded transition
-                    hover:bg-purple-700">
-                  Submit
-                </button>
-              </vee-form>
+              <app-login-form v-if="tab === 'login'" />
+              <app-register-form v-else />
             </div>
           </div>
         </div>
@@ -162,32 +54,18 @@
 
 <script>
 import { mapGetters, mapMutations } from 'vuex';
+import AppLoginForm from './Login.vue';
+import AppRegisterForm from './Registration.vue';
 
 export default {
   name: 'Auth',
+  components: {
+    AppLoginForm,
+    AppRegisterForm,
+  },
   data() {
     return {
       tab: 'login',
-      schema: {
-        name: 'required|min:3|max:100|alpha_spaces',
-        email: 'required|min:3|max:100|email',
-        age: 'required|min_value:10|max_value:100',
-        password: 'required|min:3|max:100',
-        confirm_password: 'passwords_mismatch:@password',
-        country: 'required|country_excluded:Pakistan',
-        tos: 'tos',
-      },
-      userData: {
-        country: 'USA',
-      },
-      reg_in_submission: false,
-      reg_show_alert: false,
-      reg_alert_variant: 'bg-blue-500',
-      reg_alert_msg: 'Please wait! Your account is being created.',
-      loginSchema: {
-        email: 'required|email',
-        password: 'required|min:8|max:36',
-      },
     };
   },
   computed: {
@@ -195,18 +73,6 @@ export default {
   },
   methods: {
     ...mapMutations(['toggleAuthModal']),
-    register(values) {
-      this.reg_in_submission = true;
-      this.reg_show_alert = true;
-      this.reg_alert_variant = 'bg-blue-500';
-      this.reg_alert_msg = 'Please wait! Your account is being created.';
-
-      this.reg_alert_variant = 'bg-green-500';
-      this.reg_alert_msg = 'Success! Your account has been created.';
-    },
-    login(values) {
-      console.log(values);
-    },
   },
 };
 </script>
---
+++ Desktop/music-app/src/includes/firebase.js
@@ -0,0 +1,14 @@
+import firebase from 'firebase/app';
+import 'firebase/auth';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyBO01JnJwUKl0A7p5hIbCWULieaZTs5ClE',
+  authDomain: 'vue-music-app-6a02c.firebaseapp.com',
+  projectId: 'vue-music-app-6a02c',
+  storageBucket: 'vue-music-app-6a02c.appspot.com',
+  messagingSenderId: '913958897249',
+  appId: '1:913958897249:web:7810f82997313d24ef4c20',
+};
+
+// Initialize Firebase
+export default firebase.initializeApp(firebaseConfig);
