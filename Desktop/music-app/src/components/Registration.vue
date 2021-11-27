8f89e7aee0e4045fd1c0c6fe66c02e6976a1375f
= <olaidemmanuel1@gmail.com>
2021-11-27 06:15:18 +0100

    email and password config with firebase not fixed

--- Desktop/music-app/src/components/Registration.vue
+++ Desktop/music-app/src/components/Registration.vue
@@ -114,12 +114,24 @@ export default {
       this.reg_alert_variant = 'bg-blue-500';
       this.reg_alert_msg = 'Please wait! Your account is being created.';
 
-      const userCred - await firebase.auth().createUserWithEmailAndPassword({
-        values.email, value.password
-      });
+      // Email and password auth using firebase
+      let userCredentials = null;
+      try {
+        userCredentials = await firebase.auth().createUserWithEmailAndPassword(
+          values.email,
+          values.password,
+        );
+      } catch (error) {
+        this.reg_in_submission = false;
+        this.reg_alert_variant = 'bg-red-500';
+        this.reg_alert_msg = 'An unexpected error occured, Please try again later.';
+        // to stop the function from running after the error
+        return;
+      }
+
       this.reg_alert_variant = 'bg-green-500';
       this.reg_alert_msg = 'Success! Your account has been created.';
-      console.log(values);
+      console.log(userCredentials);
     },
   },
 };
